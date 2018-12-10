6f787bae5171ab851a503be3453923084144205c
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-12-10 08:13:46 +0200

    Add fucking stupid zadacha

---
+++ Informatics/Zadacha M1-45/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadacha_M1_45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int m = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] < m) {
+                    arr[i] = 2 * arr[i];
+                }
+                if (arr[i] == m) {
+                    arr[i] = -arr[i];
+                }
+
+                if(arr[i] > m) {
+                    arr[i] = arr[i] - 1;
+                }
+            }
+        }
+    }
+}
